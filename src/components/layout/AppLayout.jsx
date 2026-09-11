4228cb1111c71c64b3f8583a43db97d76147aa75
Eagle RC <charanpasapu@gmail.com>
2026-09-11 15:39:29 +0530

    Refactor AppLayout to include Navbar and MobileNavigation
    Added Navbar and MobileNavigation components, integrated theme context.

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,17 +1,27 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
-import AppShell from "./AppShell";
+import Navbar from "./Navbar";
+import MobileNavigation from "./MobileNavigation";
+import { useTheme } from "../../context/ThemeContext";
 
 function AppLayout() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="flex min-h-screen bg-white text-[#171717] dark:bg-[#090909] dark:text-white">
+    <div className="flex min-h-screen bg-white text-[#171717] dark:bg-[#090909] dark:text-white transition-colors duration-300">
       <Sidebar />
 
-      <main className="min-w-0 flex-1">
-        <AppShell>
-          <Outlet />
-        </AppShell>
-      </main>
+      <div className="flex min-w-0 flex-1 flex-col">
+        <Navbar theme={theme} onToggleTheme={toggleTheme} />
+
+        <main className="flex-1 px-4 py-6 sm:px-6 lg:px-8 xl:px-10 pb-28 lg:pb-12">
+          <div className="mx-auto max-w-7xl">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+
+      <MobileNavigation />
     </div>
   );
 }
